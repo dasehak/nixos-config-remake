d3783ae90eb1294459cc468db52e74f5335b4452
Vasiliy Petrov <dasehak@disroot.org>
2024-07-22 18:17:40 +0300

    vscode home-manager config

--- home/modules/vscode/default.nix
+++ home/modules/vscode/default.nix
@@ -17,6 +17,29 @@ in {
   config = mkIf cfg.enable {
     programs.vscode = {
       enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        ms-vscode.cpptools
+        ms-vscode.cpptools-extension-pack
+        ms-vscode.cmake-tools
+        twxs.cmake
+        catppuccin.catppuccin-vsc
+        catppuccin.catppuccin-vsc-icons
+        eamodio.gitlens
+        bbenoist.nix
+      ];
+
+      userSettings = {
+        files = {
+          trimTrailingWhitespace = true;
+          autoSave = "afterDelay";
+        };
+        workbench = {
+          iconTheme = "catppuccin-perfect-mocha";
+          colorTheme = "Catppuccin Mocha";
+        };
+        git.autofetch = true;
+        window.titleBarStyle = "custom";
+      };
     };
   };
 }
