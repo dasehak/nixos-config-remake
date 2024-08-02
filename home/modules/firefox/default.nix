af705f62868df792e581885b7f4fed5c6718cb6a
Vasiliy Petrov <dasehak@disroot.org>
2024-08-02 17:25:50 +0300

    fix 4get url

--- home/modules/firefox/default.nix
+++ home/modules/firefox/default.nix
@@ -212,7 +212,7 @@ in {
           default = "4get.ducks.party";
           engines = {
             "4get.ducks.party" = {
-              urls = [{ template = "https://4get.ducks.party/api/v1/ac?s={searchTerms}&scraper=google"; }];
+              urls = [{ template = "https://4get.ducks.party/web?s={searchTerms}"; }];
               iconUpdateURL = "https://4get.ducks.party/favicon.ico";
               updateInterval = 24 * 60 * 60 * 1000;
             };
