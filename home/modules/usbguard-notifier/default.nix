f2d3c0819edf756fd4f705962fb65bf0d95bd084
dasehak <dasehak@disroot.org>
2024-11-18 00:31:02 +0300

    bruh

---
+++ home/modules/usbguard-notifier/default.nix
@@ -0,0 +1,31 @@
+{ pkgs
+, lib
+, config
+, ...
+}:
+
+with lib;
+
+let
+  cfg = config.module.usbguard-notifier;
+in {
+  options = {
+    module.usbguard-notifier.enable = mkEnableOption "Enables usbguard-notifier";
+  };
+
+  config = mkIf cfg.enable {
+    systemd.user.services.usbguard-notifier = {
+      Unit = {
+        Description = "Starts usbguard-notifier.";
+        After = [ "usbguard.service" ];
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.usbguard-notifier}/bin/usbguard-notifier";
+      };
+    };
+  };
+}
+
